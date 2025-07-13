8dd382b865e9e5491b401e28231e8df722e2ff0b
unknown <quanminh0506@gmail.com>
2025-07-13 10:12:30 +0700

    a

---
+++ dao.py
@@ -0,0 +1,8 @@
+import data,json
+def auth_user(username, password):
+    with open("../data/data.json", encoding="utf-8") as f:
+        users= json.load(f)
+        for u in users:
+            if u["username"]==username and u["password"]== password:
+                return True
+    return False
